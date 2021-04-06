75387cd1252c4ec033fad1f25f26974441e1ae65
Fredrik Vernblom <fredrikvernblom@gmail.com>
2021-04-06 08:57:21 +0200

    Initial push

---
+++ app.js
@@ -0,0 +1,44 @@
+function initMap() {
+
+  let options = {
+
+    center: { lat: -37.814, lng: 144.96332 },
+    zoom: 10
+  }
+
+
+  map = new google.maps.Map(document.getElementById('map'), {
+
+    options
+  });
+
+
+
+  let infoWindow = new google.maps.InfoWindow({
+    content: "Click the map to get price estimates!",
+    position: { lat: -37.814, lng: 144.96332 },
+  });
+
+  let form = {
+
+    
+  }
+
+  infoWindow.open(map);
+  map.addListener("click", (mapsMouseEvent) => {
+    infoWindow.close();
+    
+    infoWindow = new google.maps.InfoWindow({
+      position: mapsMouseEvent.latLng,
+    });
+
+
+
+    infoWindow.setContent(
+      JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
+      //"Test"
+    );
+    infoWindow.open(map);
+  });
+
+}
