e457f2a36e29032eb88dc6dce712328dfc8b1289
pellehardstedt <pelle.hardstedt>
2021-04-07 10:49:07 +0200

    model training

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from sanic import Sanic
 from sanic.response import json
 
+from XGBoost_training import train, predict
+
+# train()
+# print(predict([1.000000, 0.000000, 0.000000, 2.000000, 2.500000, 1.000000,
+#        1.000000, 202.000000, 156.834586, 1966.609153, -37.799600,
+#        144.998400, 4019.000000, 9.000000]))
+
 app = Sanic('Melbourne Housing')
 
-app.static('/', './dist/index.html')
+app.static('/', './dist/')
 
-if __name__ == "__main__":
+@app.post('/api/prediction')
+async def handler(req):
+  print(req.json)
+
+if __name__ == '__main__':
   app.run(port=8000)
