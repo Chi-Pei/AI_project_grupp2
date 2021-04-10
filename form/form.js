f049e63f22fe39d8e79683cffc5aeebf0510fa7b
Chi-Pei <72336436+Chi-Pei@users.noreply.github.com>
2021-04-10 19:44:52 +0200

    minor changes

--- form/form.js
+++
@@ -1,9 +0,0 @@
-function adressCheck() {
-  if (document.getElementById('input-streetAdress').addEventListener('submit', true)) {
-    document.getElementById('input-lat').style.display = "none"
-    document.getElementById('input-long').style.display = "none"
-  } else {
-    document.getElementById('input-lat').style.display = "block"
-    document.getElementById('input-long').style.display = "block"
-  }
-}
